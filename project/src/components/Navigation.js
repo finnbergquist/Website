e9c4ae508c027a845fbbc0eaad969bd898c2fabf
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-04-01 16:47:33 -0500

    WIP: Created styles folder for css files. Updated Nav bar.

--- project/src/components/Navigation.js
+++ project/src/components/Navigation.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 
+// style 
+import '../styles/Nav.css';
+
 function Navigation() {
     return (
-        <div>
-            <div>
-                <Link to='/'>Home</Link>
-            </div>
-            <div>
-                <Link to='/Progress-Feed'>Progress Feed</Link>
-            </div>
-            <div>
-                <Link to='/Project-History'>History</Link>
-            </div>
-            <div>
-                <Link to='/About'>About</Link>
+        <div id="nav-container">
+            <h2>Project Name</h2>
+
+            <div className="link-container">
+                <div className="link-box">
+                    <Link to='/' className="link">Home</Link>
+                </div>
+                <div className="link-box">
+                    <Link to='/Progress-Feed' className="link">Progress Feed</Link>
+                </div>
+                <div className="link-box">
+                    <Link to='/Project-History' className="link">History</Link>
+                </div>
+                <div className="link-box">
+                    <Link to='/About' className="link">About</Link>
+                </div>
             </div>
         </div>
     );
