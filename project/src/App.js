326192c689916020243deb94310c81281923bee9
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-04-09 02:04:54 -0500

    WIP: Cards created for artwork display. Images pulled form website.

--- project/src/App.js
+++ project/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
-import Feed from './components/Feed'; 
+import Database from './components/Database'; 
 import PHistory from './components/History'; 
 import About from './components/About'; 
 import Navigation from './components/Navigation';
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Navigation/>
       <Route path="/" exact component={Home}></Route>
-      <Route path="/Database" component={Feed}></Route>
+      <Route path="/Database" component={Database}></Route>
       <Route path="/Project-History" component={PHistory}></Route>
       <Route path="/About" component={About}></Route>
       <Route path="/Art-Collection" component={ArtList}></Route>
