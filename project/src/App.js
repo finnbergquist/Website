4e84fcd8db9c122d7706eaecdcf07ad1f12634c4
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-04-09 02:46:42 -0500

    WIP: Login and SignUp forms created. Hook warnings for both.

--- project/src/App.js
+++ project/src/App.js
@@ -7,6 +7,8 @@ import PHistory from './components/History';
 import About from './components/About'; 
 import Navigation from './components/Navigation';
 import ArtList from './components/ArtList';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
       <Route path="/Project-History" component={PHistory}></Route>
       <Route path="/About" component={About}></Route>
       <Route path="/Art-Collection" component={ArtList}></Route>
+      <Route path="/Login" component={Login}></Route>
+      <Route path="/Register" component={SignUp}></Route>
     </div>
   );
 }
