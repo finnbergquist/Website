9d9d636ee14abdb112d8c4c96086327c316a0c59
finnbergquist <fbergqui@bowdoin.edu>
2020-04-15 15:20:14 -0400

    History Updates

--- project/src/components/History.js
+++ project/src/components/History.js
@@ -5,6 +5,8 @@ import '../styles/about.css';
 //images
 import Blueprint from '../images/blueprint.png'
 import GalleryPhoto from '../images/gallery.png'
+import TeamPhoto from '../images/DemoDay.png'
+import Database from '../images/Database.png'
 
 function PHistory() {
     return (
@@ -20,17 +22,28 @@ function PHistory() {
                 <h3>Starting the Database</h3>
                 
                 <p>
-                Last years team did a great job starting off this project. One of their first
-                steps was picking the room to implement this project within.
+                Last years team started this project. Their core tasks included UX/UI, data collection, 
+                database design, application development, computer tracking, and voice interface. The 
+                students were divided up into teams to work on each task. 
                 </p>
-                <img src = {GalleryPhoto} alt='photo of gallery'></img>
+                <img src = {TeamPhoto} alt='photo of gallery'></img>
                 
                 <p>
-                text 2
+                They accomplished a lot but ran into some problems on the way. One of these included 
+                changing their entire database structure creating compatibility complications amongst
+                all the teams. The course's core curriculum is focused on database design, and this 
+                project truly reinforces that. This is a visual depiction of their database.
                 </p>
-                <p>
-                text 3
-                </p>
+                <img src = {Database} alt='photo of gallery'></img>
+            </TimelineItem>
+
+            <TimelineItem
+                key="002"
+                dateText="Summer 2019"                
+                style={{color: '#fff'}}                                        
+            >
+                <h3>Professor Doore's Summer Research</h3>
+                
             </TimelineItem>
         </Timeline> 
         </div>
